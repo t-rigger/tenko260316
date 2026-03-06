4a7056ca1847b2565e27f844415f30a2675e735b
安里直人 <asatonaoto@asatoyuinoMacBook-Air.local>
2026-03-06 14:35:31 +0900

    fix(functions): explicitly require v1 functions to ensure gen 1 deployment

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,4 @@
-const functions = require("firebase-functions");
+const functions = require("firebase-functions/v1");
 const crypto = require("crypto");
 const fs = require("fs");
 const path = require("path");
